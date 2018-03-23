ec01dff2282a474edd0a4c7afc813a0fda3b2c48
Zhao Lang <zhao.lang@sas.com>
2018-03-23 09:59:31 -0400

    add option to use async incrementation

--- tokenapi.go
+++ tokenapi.go
@@ -15,7 +15,7 @@ type TokenAPI interface {
 
 // UnaryServerInterceptor returns a new unary server interceptor that
 // checks token validity per-request and increments usage if valid.
-func UnaryServerInterceptor(tokenAPI TokenAPI) grpc.UnaryServerInterceptor {
+func UnaryServerInterceptor(tokenAPI TokenAPI, asyncInc bool) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		allowed, err := tokenAPI.CheckValidity(ctx)
 		if err != nil {
@@ -30,13 +30,16 @@ func UnaryServerInterceptor(tokenAPI TokenAPI) grpc.UnaryServerInterceptor {
 			return nil, e
 		}
 
-		go func() {
-			tokenAPI.IncrementUsage(ctx)
-		}()
-		// _, err = tokenAPI.IncrementUsage(ctx)
-		// if err != nil {
-		// 	return nil, err
-		// }
+		if asyncInc {
+			go func() {
+				tokenAPI.IncrementUsage(ctx)
+			}()
+		} else {
+			_, err = tokenAPI.IncrementUsage(ctx)
+			if err != nil {
+				return nil, err
+			}
+		}
 
 		return resp, e
 	}
@@ -44,7 +47,7 @@ func UnaryServerInterceptor(tokenAPI TokenAPI) grpc.UnaryServerInterceptor {
 
 // StreamServerInterceptor returns a new stream server interceptor that
 // checks token validity per-request and increments usage if valid.
-func StreamServerInterceptor(tokenAPI TokenAPI) grpc.StreamServerInterceptor {
+func StreamServerInterceptor(tokenAPI TokenAPI, asyncInc bool) grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		allowed, err := tokenAPI.CheckValidity(ss.Context())
 		if err != nil {
@@ -59,13 +62,16 @@ func StreamServerInterceptor(tokenAPI TokenAPI) grpc.StreamServerInterceptor {
 			return e
 		}
 
-		go func() {
-			tokenAPI.IncrementUsage(ss.Context())
-		}()
-		// _, err = tokenAPI.IncrementUsage(ss.Context())
-		// if err != nil {
-		// 	return err
-		// }
+		if asyncInc {
+			go func() {
+				tokenAPI.IncrementUsage(ss.Context())
+			}()
+		} else {
+			_, err = tokenAPI.IncrementUsage(ss.Context())
+			if err != nil {
+				return err
+			}
+		}
 
 		return e
 	}
